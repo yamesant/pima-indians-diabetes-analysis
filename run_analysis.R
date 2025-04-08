c8661d56096e655dba7be8d115b8b1d2b08a3b8a
yamesant <165981998+yamesant@users.noreply.github.com>
2025-04-08 16:43:11 +1000

    Do predictive modelling

--- run_analysis.R
+++ run_analysis.R
@@ -47,3 +47,58 @@ ggsave("./plots/exploratory-1.png")
 data <- clean(data)
 visualise(data)
 ggsave("./plots/exploratory-2.png")
+
+# Predictive Modelling Setup ----------------------------------------------
+
+data_split <- initial_split(data, prop = 0.75, strata = is_diabetic)
+data_train <- training(data_split)
+data_test <- testing(data_split)
+
+data_recipe <- recipe(is_diabetic ~ ., data = data_train)
+
+metrics <- metric_set(accuracy, precision, recall)
+
+# Decision Tree -----------------------------------------------------------
+
+specification <- decision_tree() |> 
+  set_mode("classification")
+
+workflow <- workflow() |> 
+  add_recipe(data_recipe) |> 
+  add_model(specification)
+
+fit <- workflow |> 
+  fit(data = data_train)
+
+augment(fit, new_data = data_test) |> 
+  conf_mat(truth = is_diabetic, estimate = .pred_class) |> 
+  autoplot(conf_matrix, type = "heatmap") +
+  labs(title = "Random Tree Confusion Matrix") +
+  theme(plot.title = element_text(hjust = 0.5))
+ggsave('./plots/random-tree-confusion-matrix.png')
+
+augment(fit, data_test) |>
+  metrics(truth = is_diabetic, estimate = .pred_class)
+
+# Random Forest -----------------------------------------------------------
+
+specification <- rand_forest() |> 
+  set_mode("classification") |> 
+  set_engine("ranger")
+
+workflow <- workflow() |> 
+  add_recipe(data_recipe) |> 
+  add_model(specification)
+
+fit <- workflow |> 
+  fit(data = data_train)
+
+augment(fit, new_data = data_test) |> 
+  conf_mat(truth = is_diabetic, estimate = .pred_class) |> 
+  autoplot(conf_matrix, type = "heatmap") +
+  labs(title = "Random Forest Confusion Matrix") +
+  theme(plot.title = element_text(hjust = 0.5))
+ggsave('./plots/random-forest-confusion-matrix.png')
+
+augment(fit, data_test) |>
+  metrics(truth = is_diabetic, estimate = .pred_class)
